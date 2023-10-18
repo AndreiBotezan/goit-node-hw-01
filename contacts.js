d93808865faa13b12952076d6c8591ce7f922428
AndreiBotezan <andreibotezan@gmail.com>
2023-10-18 17:31:07 +0300

    try/catch blocks added

--- contacts.js
+++ contacts.js
@@ -9,36 +9,44 @@ const __dirname = dirname(__filename);
 const contactsPath = join(`${__dirname}`, "./db/contacts.json");
 
 export const listContacts = async () => {
-	const contacts = await fs.readFile(contactsPath);
-	return JSON.parse(contacts);
+	try {
+		const contacts = await fs.readFile(contactsPath);
+		return JSON.parse(contacts);
+	} catch (error) {
+		console.warn(error);
+	}
 };
 
 export const getContactById = async (contactId) => {
-	const contacts = await listContacts();
-	const contact = contacts.find((el) => el.id === contactId);
-	return contact || null;
+	try {
+		const contacts = await listContacts();
+		const contact = contacts.find((el) => el.id === contactId);
+		return contact;
+	} catch (error) {
+		console.warn(error);
+	}
 };
 
 export const removeContact = async (contactId) => {
-	const contacts = await listContacts();
-	const index = contacts.findIndex((el) => el.id === contactId);
-
-	if (index === -1) {
-		return null;
+	try {
+		const contacts = await listContacts();
+		const index = contacts.findIndex((el) => el.id === contactId);
+		const [removeItem] = contacts.splice(index, 1);
+		await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+		return removeItem;
+	} catch (error) {
+		console.warn(error);
 	}
-
-	const [removeItem] = contacts.splice(index, 1);
-
-	await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-
-	return removeItem;
 };
 
 export const addContact = async (data) => {
-	const contacts = await listContacts();
-	const newContact = { id: nanoid(), ...data };
-	contacts.push(newContact);
-	await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-
-	return newContact;
+	try {
+		const contacts = await listContacts();
+		const newContact = { id: nanoid(), ...data };
+		contacts.push(newContact);
+		await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+		return newContact;
+	} catch (error) {
+		console.warn(error);
+	}
 };
